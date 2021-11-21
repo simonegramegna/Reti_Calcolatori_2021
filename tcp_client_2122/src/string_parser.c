3afc9148836bebdd5ab194fa89f9b11ae318bb62
simone_pc <gramegna.simone@gmail.com>
2021-11-21 16:33:27 +0100

    modifiche su funzione di parsing

--- tcp_client_2122/src/string_parser.c
+++ tcp_client_2122/src/string_parser.c
@@ -1,8 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 #include "string_parser.h"
+
+#define PLUS 43
+#define MINUS 45
+
+int check_limits(int num)
+{
+    int check;
+    check = 0;
+
+    if (num > 0 && num > INT_MAX - num)
+    {
+        check = 1;
+    }
+    else
+    {
+        if (num < 0 && num < INT_MIN - num)
+        {
+            check = 1;
+        }
+    }
+
+    return check;
+}
+
+int contain_only_numbers(char *str)
+{
+    int i;
+    int contain_numbers;
+
+    i = 0;
+    contain_numbers = 1;
+
+    while (str[i] != '\0' && contain_numbers == 1)
+    {
+        if (!isdigit(str[i]))
+        {
+            if ((str[i] != MINUS && str[i] != PLUS))
+            {
+                contain_numbers = 0;
+            }
+
+            if ((str[i] == MINUS || str[i] == PLUS) && (i > 0))
+            {
+                contain_numbers = 0;
+            }
+        }
+
+        i++;
+    }
+
+    return contain_numbers;
+}
+
+int is_valid_integer(char *str)
+{
+    int valid;
+    valid = 1;
+
+    if (contain_only_numbers(str) == 1)
+    {
+        int str_num;
+        str_num = atoi(str);
+
+        if (check_limits(str_num) == 1)
+        {
+            valid = 0;
+        }
+    }
+    else
+    {
+        valid = 0;
+    }
+    return valid;
+}
 
 int correct_operation(char operator)
 {
@@ -39,13 +114,7 @@ int valid_input(char *user_input)
         }
         else
         {
-            int str_num;
-            str_num = atoi(input_token);
-
-            if (str_num == 0 && strlen(input_token) != 1 && input_token[0] != '0')
-            {
-                valid = 0;
-            }
+            valid = is_valid_integer(input_token);
         }
         input_token = strtok(NULL, " ");
     }
@@ -81,7 +150,6 @@ math_message get_math_message(char *user_input)
         }
         if (token_counter == 3)
         {
-
             requested_operation.n2 = atoi(input_token);
         }
         input_token = strtok(NULL, " ");
